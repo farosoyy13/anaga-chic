1ad384b74bb130387334584657a4c59bc4f8d852
farosoyy13 <156523277+farosoyy13@users.noreply.github.com>
2026-06-10 22:07:43 +0300

    Update firebaseconfig.ts

--- src/firebaseconfig.ts
+++ src/firebaseconfig.ts
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth"; // أضفنا أدوات الدخول
-import { getFirestore } from "firebase/firestore"; // أضفنا أدوات قاعدة البيانات
-import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // هذه الأداة ضرورية
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCK8IWcEV_z9DXj0YKq8IjObm1AzEJsjgE",
   authDomain: "anaqachic-5.firebaseapp.com",
@@ -15,11 +12,8 @@ const firebaseConfig = {
   measurementId: "G-PZSBZ0BKZN"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// تصدير الأدوات لكي تستخدمها في ملفات المشروع
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider(); // هذا هو مفتاح الدخول بجوجل
-export const analytics = getAnalytics(app);
+export const db = getFirestore(app); // هنا قمنا بتهيئة قاعدة البيانات
+export const googleProvider = new GoogleAuthProvider();
