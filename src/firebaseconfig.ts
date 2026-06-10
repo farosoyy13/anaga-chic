ba76671d5f5acb18f3d8170bbe4ccc57e85c3840
farosoyy13 <156523277+farosoyy13@users.noreply.github.com>
2026-06-10 18:51:29 +0300

    Update firebaseconfig.ts

--- src/firebaseconfig.ts
+++ src/firebaseconfig.ts
@@ -1,17 +1,23 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
+// إعدادات Firebase الخاصة بمشروعك
 const firebaseConfig = {
-  apiKey: "AIzaSyCK8IWcEV_z9DXj0YKq8IjObm1AzI",
+  apiKey: "AIzaSyCK8IWcEV_z9DXj0YKq8IjObm1AzEJsjgE",
   authDomain: "anaqachic-5.firebaseapp.com",
   projectId: "anaqachic-5",
   storageBucket: "anaqachic-5.firebasestorage.app",
   messagingSenderId: "403484478286",
-  appId: "1:403484478286:web:1ce25b87ea7ebec886d38e"
+  appId: "1:403484478286:web:1ce25b87ea7ebec8ddec1f",
+  measurementId: "G-PZSBZ0BKZN"
 };
 
+// تهيئة Firebase
 const app = initializeApp(firebaseConfig);
 
+// تصدير الأدوات اللازمة للمشروع
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
